3fbdf3d6e390e248a0c42aa5e830bf4afe144d35
Abin Anto <abin98anto@gmail.com>
2024-09-26 10:05:08 +0530

    Register User completed
    Completed the implementation of the register user feature.
    Also installed nodemon.

--- src/server.ts
+++ src/server.ts
@@ -9,16 +9,17 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 
 connectDB();
 
-app.post("/api/auth/register", registerUser);
+app.post("/register", registerUser);
 app.post("/api/auth/login", loginUser);
 
 app.get("/api/users", protect, getUsers);
 app.delete("/api/users/:id", protect, deleteUser);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
